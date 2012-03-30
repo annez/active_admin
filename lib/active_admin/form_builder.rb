75332fd123092027cf64119704b53f7e9a5f7cfd
David Annez <annez@tictocfamily.com>
2012-03-30 16:33:40 +0100

    Fixed double nested forms

--- lib/active_admin/form_builder.rb
+++ lib/active_admin/form_builder.rb
@@ -66,18 +66,20 @@ module ActiveAdmin
         template.content_tag :div, :class => "has_many #{association}" do
           form_buffers.last << template.content_tag(:h3, association.to_s.titlecase)
           inputs options, &form_block
-
+          model_name = association.to_s.singularize.titlecase
+          place_holder = "NEW_#{model_name}_RECORD"
           # Capture the ADD JS
           js = with_new_form_buffer do
             inputs_for_nested_attributes  :for => [association, object.class.reflect_on_association(association).klass.new],
                                           :class => "inputs has_many_fields",
                                           :for_options => {
                                             :child_index => "NEW_RECORD"
+                                           :child_index => place_holder
                                           }, &form_block
           end
 
           js = template.escape_javascript(js)
-          js = template.link_to I18n.t('active_admin.has_many_new', :model => association.to_s.singularize.titlecase), "#", :onclick => "$(this).before('#{js}'.replace(/NEW_RECORD/g, new Date().getTime())); return false;", :class => "button new_button"
+          js = template.link_to I18n.t('active_admin.has_many_new', :model => model_name), "#", :onclick => "$(this).before('#{js}'.replace(/#{place_holder}/g, new Date().getTime())); return false;", :class => "button new_button"
 
           form_buffers.last << js.html_safe
         end
